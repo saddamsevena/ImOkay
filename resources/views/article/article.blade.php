9675b52ba1412c2a7aebec5c835b361a93b7f80b
Fadlan Fasya <fasyafadlan7@gmail.com>
2021-11-08 11:17:28 +0700

    update artikel again

--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -1,116 +1,162 @@
 @extends('layouts.app')
 
 @section('content')
-<link href="css/article.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="css/article.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+    <!-- <meta name="generator" content="Nicepage 3.29.1, nicepage.com">
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website"> -->
 
-<div class="w3-container">
-  <h2>Topik Terkini</h2>
-</div>
 
-<div class="w3-content w3-display-container" style="max-width:800px">
-  <img class="mySlides" src="img/panjang.png" style="width:100%;">
-  <img class="mySlides" src="img/panjang.png" style="width:100%">
-  <img class="mySlides" src="img/panjang.png" style="width:100%">
-  <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
-    <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
-    <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
-  </div>
-</div>
-
-<br><br><br><br><br>
-
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="img/merenung.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Merenung</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <body class="u-body">
+    <section class="u-align-center u-clearfix u-section-1" id="sec-ff6a">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">Topik Terkini</h2>
+        <div id="carousel-bd35" data-interval="5000" data-u-ride="carousel" class="u-carousel u-expanded-width u-slider u-slider-1">
+          <ol class="u-absolute-hcenter u-carousel-indicators u-hidden u-carousel-indicators-1">
+            <li data-u-target="#carousel-bd35" class="u-active u-grey-30" data-u-slide-to="0"></li>
+            <li data-u-target="#carousel-bd35" class="u-grey-30" data-u-slide-to="1"></li>
+          </ol>
+          <div class="u-carousel-inner" role="listbox">
+            <div class="u-active u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-2">
+                <img alt="" class="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xl u-image u-image-default u-image-1" data-image-width="1280" data-image-height="716" src="img/1.jpeg">
+                <h4 class="u-align-center u-text u-text-default u-text-2">Sample Headline</h4>
+                <p class="u-align-center u-text u-text-5">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+            <div class="u-align-center u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-valign-top u-container-layout-2">
+                <img alt="" class="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xl u-image u-image-default u-image-2" data-image-width="1280" data-image-height="716" src="img/1.jpeg">
+                <h4 class="u-text u-text-default u-text-4">Sample Headline</h4>
+                <p class="u-text u-text-5">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+            <div class="u-align-center u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-valign-top u-container-layout-2">
+                <img alt="" class="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xl u-image u-image-default u-image-2" data-image-width="1280" data-image-height="716" src="img/1.jpeg">
+                <h4 class="u-text u-text-default u-text-4">Sample Headline</h4>
+                <p class="u-text u-text-5">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+          <a class="u-carousel-control u-carousel-control-prev u-grey-60 u-icon-circle u-spacing-9 u-carousel-control-1" href="#carousel-bd35" role="button" data-u-slide="prev">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
+                    c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path></svg>
+            </span>
+            <span class="sr-only">+Previous</span>
+          </a>
+          <a class="u-carousel-control u-carousel-control-next u-grey-60 u-icon-circle u-spacing-9 u-carousel-control-2" href="#carousel-bd35" role="button" data-u-slide="next">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5
+                    c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path></svg>
+            </span>
+            <span class="sr-only">+Next</span>
+          </a>
+        </div>
+        <h3 class="u-text u-text-default u-text-6"><b>berita</b> terbaru</h3>
+        <p class="u-align-left u-text u-text-7">Sample text.</p>
+        <div class="u-blog u-expanded-width u-blog-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-repeater-item-1">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-3">
+                <a class="u-post-header-link" href="blog/post-5.html">
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-3" src="img/3.jpeg" data-image-width="809" data-image-height="1080">
+                </a>
+                <h4 class="u-blog-control u-text u-text-8">
+                  <a class="u-post-header-link" href="blog/post-5.html">Post 6 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-1">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0)</span>
+                </div>
+                <a href="blog/post-5.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-1">Read More</a>
+              </div>
+            </div>
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-video-cover u-repeater-item-2">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-4">
+                <a class="u-post-header-link" href="blog/post-4.html">
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-4" src="img/4.jpeg" data-image-width="1125" data-image-height="750">
+                </a>
+                <h4 class="u-blog-control u-text u-text-9">
+                  <a class="u-post-header-link" href="blog/post-4.html">Post 5 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-2">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0) </span>
+                </div>
+                <a href="blog/post-4.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-2">Read More</a>
+              </div>
+            </div>
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-video-cover u-repeater-item-3">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-5" src="img/5.jpeg" data-image-width="1199" data-image-height="1500">
+                </a>
+                <h4 class="u-blog-control u-text u-text-10">
+                  <a class="u-post-header-link" href="blog/post-3.html">Post 4 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-3">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0)</span>
+                </div>
+                <a href="blog/post-3.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-3">Read More</a>
+              </div>
+            </div>
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-video-cover u-repeater-item-3">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-5" src="img/merenung.jpg" data-image-width="1199" data-image-height="1500">
+                </a>
+                <h4 class="u-blog-control u-text u-text-10">
+                  <a class="u-post-header-link" href="blog/post-3.html">Post 4 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-3">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0)</span>
+                </div>
+                <a href="blog/post-3.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-3">Read More</a>
+              </div>
+            </div>
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-video-cover u-repeater-item-3">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-5" src="img/pager.jpg" data-image-width="1199" data-image-height="1500">
+                </a>
+                <h4 class="u-blog-control u-text u-text-10">
+                  <a class="u-post-header-link" href="blog/post-3.html">Post 4 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-3">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0)</span>
+                </div>
+                <a href="blog/post-3.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-3">Read More</a>
+              </div>
+            </div>
+            <div class="u-align-center u-black u-blog-post u-container-style u-repeater-item u-video-cover u-repeater-item-3">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                <a class="u-post-header-link" href="blog/post-3.html"><!--blog_post_image-->
+                  <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-5" src="img/obat.jpg" data-image-width="1199" data-image-height="1500">
+                </a>
+                <h4 class="u-blog-control u-text u-text-10">
+                  <a class="u-post-header-link" href="blog/post-3.html">Post 4 Headline</a>
+                </h4>
+                <div class="u-blog-control u-metadata u-text-grey-30 u-metadata-3">
+                  <span class="u-meta-date u-meta-icon">Mon Nov 08 2021</span>
+                  <span class="u-meta-comments u-meta-icon">Comments (0)</span>
+                </div>
+                <a href="blog/post-3.html" class="u-blog-control u-border-2 u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-none u-text-body-alt-color u-btn-3">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="img/brain.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="img/tolong.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">his is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="img/pager.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="img/note.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="img/yoga.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
-</script>
+    </section>
+  </body>
 @endsection
+
+<!-- </html> -->
