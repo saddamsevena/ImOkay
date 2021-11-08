0d6ead77f86cc062f339f8b42e5d23892802b630
Fadlan Fasya <fasyafadlan7@gmail.com>
2021-11-08 11:35:51 +0700

    halaman artikelnya

---
+++ resources/views/article/showarticle.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="css/showarticle.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+
+  <body class="u-body">
+    <section class="u-align-center u-clearfix u-section-1" id="sec-f0bf">
+
+      <div class="u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-container-style u-expanded-width u-post-details u-post-details-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <img src="img/4.jpeg">
+            <h2 class="u-blog-control u-text u-text-1">Judul Artikel</h2>
+            <div class="u-blog-control u-metadata u-metadata-1">
+              <span class="u-meta-date u-meta-icon">Nov 08, 2021</span>
+            </div>
+            <div class="u-align-justify u-blog-control u-post-content u-text u-text-2 fr-view">
+              <p>Isi Artikel. Pir adalah buah yang manis, ukuranya sedikit lebih besar dari apel dan memiliki kulit yang sedikit lebih tipis. Kulit buah pir biasanya kuning kehijauan, dan dagingnya putih dan lembut. Buah pir juga memiliki biji yang biasanya tidak bisa dimakan. 
+
+              Selain rasanya yang lezat, buah pir juga kaya akan antioksidan esensial dan serat makanan. Buah pir dikemas dengan banyak nutrisi dalam 100 kalori yang bebas lemak dan bebas kolesterol. 
+
+              Pantas saja jika buah ini sering dimasukkan dalam diet dan pola makan sehat.  Sebab mengonsumsi buah pir dapat menunjang penurunan berat badan dan mengurangi risiko terkena kanker, diabetes, dan penyakit jantung. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+@endsection
+<!-- </html> -->
