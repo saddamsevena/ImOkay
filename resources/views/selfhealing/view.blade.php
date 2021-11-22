ac18226d776bf152081503a30268f02deb9d80ae
salsaskarin <karinsalsabila17@gmail.com>
2021-11-22 20:51:35 +0700

    10 day selfhealing

--- resources/views/selfhealing/view.blade.php
+++ resources/views/selfhealing/view.blade.php
@@ -20,65 +20,65 @@
     <div class="container">
       <span class="v98_81">Day 2 - How do I control my stress ?</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=O6X3KH1TmeM&ab_channel=ClarinHayes" class="firstvideo">Second Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1OIkhiATqxuWZMrUgTZEFzK_-vEcjWg3X/view?usp=sharing" class="firstmodul">Second Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 3 - How do I control my stress ?</span>
+      <span class="v98_81">Day 3 - How do I control my insecurities ?</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=BOeh9RMlwwc&ab_channel=SatuPersen-IndonesianLifeSchool" class="firstvideo">Third Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://dosenpsikologi.com/insecure-dalam-psikologi" class="firstmodul">Third Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 4 - How do I control my stress ?</span>
+      <span class="v98_81">Day 4 - Self Healing</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=U6qeW1MoK9s&ab_channel=SatuPersen-IndonesianLifeSchool" class="firstvideo">Fourth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1ekZwKspFLwbwwp4faw1sMM2AZKM8yto-/view?usp=sharing" class="firstmodul">Fourth Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 5 - How do I control my stress ?</span>
+      <span class="v98_81">Day 5 - What is depression and how do i handle it ?</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=wr2IqS8bsS4&ab_channel=Neuron" class="firstvideo">Fifth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/17_q7nRfDgH3ioGvQqW985pau1cV-sr0m/view?usp=sharing" class="firstmodul">Fifth Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 6 - How do I control my stress ?</span>
+      <span class="v98_81">Day 6 - Difference between stress and depression</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=HbJLJY4nswA&ab_channel=ChodidjahMakarim" class="firstvideo">Sixth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://www.sehatq.com/artikel/sering-dianggap-sama-ini-perbedaan-stres-dan-depresi" class="firstmodul">Sixth Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 7 - How do I control my stress ?</span>
+      <span class="v98_81">Day 7 - How do I control my overthinking ?</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=NbIJDsAeZNU&ab_channel=SatuPersen-IndonesianLifeSchool" class="firstvideo">Seventh Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1I6yOzao_jZWRzPDM_nuQJEyQD4n9E28t/view?usp=sharing" class="firstmodul">Seventh Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 8 - How do I control my stress ?</span>
+      <span class="v98_81">Day 8 - Self Love</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=bSy4jf36sTA&ab_channel=Greatmind" class="firstvideo">Eighth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1ATQZFHfCXNrdDttaVRRvkArTx8wCnv5R/view?usp=sharing" class="firstmodul">Eighth Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 9 - How do I control my stress ?</span>
+      <span class="v98_81">Day 9 - Meditation</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=ozaFsRbcC4o&ab_channel=SatuPersen-IndonesianLifeSchool" class="firstvideo">Ninth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1VcpBwZ5CJyhGQLM7s9iYG8-_aWAxFlrx/view?usp=sharing" class="firstmodul">Ninth Day Modul</a>
     </div>
     <div class="container">
-      <span class="v98_81">Day 10 - How do I control my stress ?</span>
+      <span class="v98_81">Day 10 - Accept failure and forgive yourself</span>
       <div class="kotakbiru1"></div>
-      <a href="#" class="firstvideo">Second Day Video</a>
+      <a href="https://www.youtube.com/watch?v=sYnglFLox4U&ab_channel=SatuPersen-IndonesianLifeSchool" class="firstvideo">Tenth Day Video</a>
       <div class="kotakbiru2"></div>
-      <a href="#" class="firstmodul">Second Day Modul</a>
+      <a href="https://drive.google.com/file/d/1lB6W5m-29cfEsIBIpjoMJxbu57Res495/view?usp=sharing" class="firstmodul">Tenth Day Modul</a>
     </div>
     <div class="container">
       <span class="v98_81">Day 11 - How do I control my stress ?</span>
