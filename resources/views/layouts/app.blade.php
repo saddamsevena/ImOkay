0e9d91506b8246898ed16053aa3b62d9721fb1c4
Saddam Habibi Utomo <61404078+saddamsevena@users.noreply.github.com>
2021-11-30 12:14:35 +0700

    Merge remote-tracking branch 'origin/rellisa' into saddam

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,11 +52,7 @@
       footer {
         margin-top: auto;
       }
-<<<<<<< HEAD
-      /* @yield('css') */
-=======
       @yield('css')
->>>>>>> origin/tasia
     </style>
   </head>
 <body>
@@ -173,17 +169,10 @@
             <a href="#" class="nav">About Us</a>
           </h6>
           <p>
-<<<<<<< HEAD
             <a href="{{ url('/about') }}" class="nav">Tentang Kami</a>
           </p>
           <p>
             <a href="{{ url('/contact') }}" class="nav">Kontak Kami</a>
-=======
-            <a href="#" class="nav">Tentang Kami</a>
-          </p>
-          <p>
-            <a href="#" class="nav">Kontak Kami</a>
->>>>>>> origin/tasia
           </p>
         </div>
       </div>
